e46673c4a41a85dc11797c6546b90fb634c21066
Maheshkumar <callmeelprofessor@gmail.com>
2021-09-16 09:20:43 +0530

    Add sum-range

---
+++ practice/sum-range.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+def sum_range_iterative(upto)
+  sum = (1..upto).inject do |accumulator, value|
+    accumulator += value
+    accumulator
+  end
+  puts "Sum of Interval 1 to #{upto} using Iteration is: #{sum}"
+end
+
+def sum_range_recursive(upto, total = 0)
+  if upto <= 0
+    total
+  else
+    sum_range_recursive(upto - 1, total + upto)
+  end
+end
+
+sum_range_iterative(100)
+puts "Sum of Interval 1 to 100 using Recursion is: #{sum_range_recursive(100)}"
