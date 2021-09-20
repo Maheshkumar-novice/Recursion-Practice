1fad389c211e81e9155fa00df06f0eec6f66d38b
Maheshkumar <callmeelprofessor@gmail.com>
2021-09-20 15:40:06 +0530

    Add U versions

--- practice/nth-fibonacci.rb
+++ practice/nth-fibonacci.rb
@@ -36,3 +36,48 @@ def call_fibonacci
 end
 
 call_fibonacci
+
+# Class Pair
+class Pair
+  attr_reader :first, :second
+
+  def initialize(first, second)
+    @first = first
+    @second = second
+  end
+end
+
+def u_version1(number)
+  return Pair.new(1, 0) if number.zero?
+
+  p1 = u_version1(number - 1)
+  Pair.new(p1.first + p1.second, p1.first)
+end
+
+def u_version(number)
+  puts "U Version 1: #{u_version1(number).first}"
+end
+
+u_version(0)
+u_version(1)
+u_version(2)
+u_version(3)
+u_version(4)
+u_version(5)
+
+def u_version3(number, current, prev)
+  return current if number.zero?
+
+  u_version3(number - 1, current + prev, current)
+end
+
+def u_version2(number)
+  puts "U Version 3: #{u_version3(number, 1, 0)}"
+end
+
+u_version2(0)
+u_version2(1)
+u_version2(2)
+u_version2(3)
+u_version2(4)
+u_version2(5)
