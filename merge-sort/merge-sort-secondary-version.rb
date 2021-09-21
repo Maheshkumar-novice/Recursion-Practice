12c63c10e396e451f6026a2a6a2fe33db3e0bd14
Maheshkumar <callmeelprofessor@gmail.com>
2021-09-21 09:47:13 +0530

    Refactor

--- merge-sort/merge-sort-secondary-version.rb
+++ merge-sort/merge-sort-secondary-version.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require 'pry'
+# rubocop:disable Metrics/MethodLength, Metrics/AbcSize
+
 def merge(left_half, right_half, new_array)
   left_index = 0
   right_index = 0
@@ -44,6 +45,7 @@ def merge_sort(array)
   merge_sort(right_half)
   merge(left_half, right_half, array)
 end
+# rubocop:enable Metrics/MethodLength, Metrics/AbcSize
 
 def call_merge_sort
   array = rand(1..10).times.map { rand(200) }
--- merge-sort/merge-sort.rb
+++ merge-sort/merge-sort.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require 'pry'
+# rubocop:disable Metrics/MethodLength, Metrics/AbcSize
+
 def merge(left_half, right_half)
   new_array = []
   left_index = 0
@@ -31,6 +32,7 @@ def merge_sort(array)
   right_half = merge_sort(array[(array.length / 2)..-1])
   merge(left_half, right_half)
 end
+# rubocop:enable Metrics/MethodLength, Metrics/AbcSize
 
 def call_merge_sort
   array = rand(1..10).times.map { rand(200) }
